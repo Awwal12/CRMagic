41f4976a7315edb3499e20097f2c80130026f849
Mohammed Moahmmed Awwal <abbaroze@gmail.com>
2023-09-13 14:15:18 +0100

    login functionality done

--- Crmagic/webapp/views.py
+++ Crmagic/webapp/views.py
@@ -1,5 +1,34 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+
+@login_required(login_url='webapp:login_user')
 def home(request):
     return render(request, 'home.html')
+
+
+def login_user(request):
+   
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            print('here')
+            login(request, user)
+            messages.success(request, 'You are now logged in')
+            return render(request, 'home.html')
+        print('not here')
+        messages.success(request, 'Username or Password is incorrect')
+        return render(request, 'login.html')
+    print('not here2')
+    return render(request, 'login.html')
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'You are now logged out')
+    return redirect('webapp:login_user')
