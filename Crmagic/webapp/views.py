9130c01536815025c2425cf7b6f55b0bb0499165
Mohammed Moahmmed Awwal <abbaroze@gmail.com>
2023-09-14 12:24:16 +0100

    added registration

--- Crmagic/webapp/views.py
+++ Crmagic/webapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from .forms import SignUpForm
 # Create your views here.
 
 
@@ -30,3 +31,20 @@ def logout_user(request):
     logout(request)
     messages.success(request, 'You are now logged out')
     return redirect('webapp:login_user')
+
+def register_user(request):
+    if request.method =='POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #authenticate and login user
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request,user)
+            messages.success(request, 'You are now regesitered, Welcome!')
+            return redirect('webapp:home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form':form})
+    return render(request, 'register.html')
