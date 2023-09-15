70d86615f2eb3d4590c3657ed9377a14a063ecc6
Mohammed Moahmmed Awwal <abbaroze@gmail.com>
2023-09-15 14:48:25 +0100

    customer record page complete

--- Crmagic/webapp/views.py
+++ Crmagic/webapp/views.py
@@ -14,7 +14,7 @@ def home(request):
 
 
 def login_user(request):
-   
+
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -22,7 +22,7 @@ def login_user(request):
         if user is not None:
             login(request, user)
             messages.success(request, 'You are now logged in')
-            return render(request, 'home.html')
+            return redirect('webapp:home')
         print('not here')
         messages.success(request, 'Username or Password is incorrect')
         return render(request, 'login.html')
@@ -34,20 +34,28 @@ def logout_user(request):
     messages.success(request, 'You are now logged out')
     return redirect('webapp:login_user')
 
-def register_user(request):
-	if request.method == 'POST':
-		form = SignUpForm(request.POST)
-		if form.is_valid():
-			form.save()
-			# Authenticate and login
-			username = form.cleaned_data['username']
-			password = form.cleaned_data['password1']
-			user = authenticate(username=username, password=password)
-			login(request, user)
-			messages.success(request, "You Have Successfully Registered! Welcome!")
-			return redirect('webapp:home')
-	else:
-		form = SignUpForm()
-		return render(request, 'register.html', {'form':form})
 
-	return render(request, 'register.html', {'form':form})
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(
+                request, "You Have Successfully Registered! Welcome!")
+            return redirect('webapp:home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form': form})
+
+    return render(request, 'register.html', {'form': form})
+
+
+@login_required(login_url='webapp:login_user')
+def customer_record(request, pk):
+    customer_record = Record.objects.get(id=pk)
+    return render(request, 'c_record.html', {'customer_record': customer_record})
